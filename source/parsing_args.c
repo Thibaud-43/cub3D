92cdb48c1e98a69c2efd3c296bce9b083df75b7d
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-01-08 11:39:34 +0100

    ok

--- source/parsing_args.c
+++ source/parsing_args.c
@@ -6,26 +6,29 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:15:51 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/08 10:19:17 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/08 11:30:50 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	ft_check_args(t_parsing *parsing, int argc, char **argv)
+void		ft_check_args(t_parsing *parsing, int argc, char **argv)
 {
-	if(argc < 2)
-		message_map_invalid(parsing, "Error \n Le nombre d'argument est trop petit.\n");
-	else if(argc > 3)
-		message_map_invalid(parsing, "Error \n Le nombre d'argument est trop grand.\n");
-	else if(argc == 3 && ft_check_save_flag(argv[2]) == 0)
-		message_map_invalid(parsing, "Error \n Le troisième argument n'est pas reconnu.\n");
-	else if(ft_checks_cub_format(argv[1]) == 0)
-		message_map_invalid(parsing, "Error \n Le format du '.cub' n'est pas correcte.\n");
+	if (argc < 2)
+		message_map_invalid(parsing,
+		"Error \n Le nombre d'argument est trop petit.\n");
+	else if (argc > 3)
+		message_map_invalid(parsing,
+		"Error \n Le nombre d'argument est trop grand.\n");
+	else if (argc == 3 && ft_check_save_flag(argv[2]) == 0)
+		message_map_invalid(parsing,
+		"Error \n Le troisième argument n'est pas reconnu.\n");
+	else if (ft_checks_cub_format(argv[1]) == 0)
+		message_map_invalid(parsing,
+		"Error \n Le format du '.cub' n'est pas correcte.\n");
 }
 
-
-int	ft_checks_cub_format(char *str)
+int			ft_checks_cub_format(char *str)
 {
 	int	i;
 	int	count_dot;
@@ -36,17 +39,18 @@ int	ft_checks_cub_format(char *str)
 	{
 		if (str[i] == '.')
 			count_dot++;
-		if (str[i] == '.' && str[i + 1] == 'c' && str[i + 2] == 'u' && str[i + 3] == 'b' 
-				&& str[i+4] == 0 && count_dot == 1)
+		if (str[i] == '.' && str[i + 1] == 'c'
+				&& str[i + 2] == 'u' && str[i + 3] == 'b'
+				&& str[i + 4] == 0 && count_dot == 1)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-void	ft_all_params( t_parsing *parsing)
+void		ft_all_params(t_parsing *parsing)
 {
-	if (parsing->no_encountered == 0 | 
+	if (parsing->no_encountered == 0 |
 		parsing->so_encountered == 0 |
 		parsing->we_encountered == 0 |
 		parsing->ea_encountered == 0 |
@@ -56,15 +60,15 @@ void	ft_all_params( t_parsing *parsing)
 		parsing->resolution_encountered == 0 |
 		parsing->player_found == 0)
 	{
-		message_map_invalid(parsing, "Error\n Il manque des paramètres\n");
+		message_map_invalid(parsing,
+		"Error\n Il manque des paramètres\n");
 	}
 }
 
-int	ft_check_save_flag(char *str)
+int			ft_check_save_flag(char *str)
 {
 	if (str[0] == '-' && str[1] == '-' && str[2] == 's' &&
 			str[3] == 'a' && str[4] == 'v' && str[5] == 'e' && str[6] == 0)
 		return (1);
 	return (0);
 }
-
--- source/parsing_ceiling.c
+++ source/parsing_ceiling.c
@@ -6,41 +6,51 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:09:14 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/08 10:11:30 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/08 11:39:27 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	ft_parse_ceiling(t_parsing *parsing)
+void		ft_parse_ceiling_2(t_parsing *parsing, char **ceiling, char **rgb)
+{
+	if ((!rgb[1]) | (rgb[2] != 0))
+		message_map_invalid(parsing,
+		"Error \n La ligne de param CEILING n'est pas bien rédigée \n");
+	rgb_tab[0] = ft_atoi(rgb[1]);
+	rgb_tab[1] = ft_atoi(ceiling[1]);
+	rgb_tab[2] = ft_atoi(ceiling[2]);
+	if ((rgb_tab[0] < 0 | rgb_tab[0] > 255)
+		| (rgb_tab[1] < 0 | rgb_tab[1] > 255)
+		| (rgb_tab[2] < 0 | rgb_tab[2] > 255))
+		message_map_invalid(parsing,
+		"Erro\n Le code rb n'est pas valide \n");
+}
+
+int			ft_parse_ceiling(t_parsing *parsing)
 {
 	char	**ceiling;
 	char	**rgb;
-	int	rgb_tab[3];
-	int	i;
-	int	k;
+	int		rgb_tab[3];
+	int		i;
+	int		k;
 
 	i = 0;
 	k = 0;
 	parsing->param_line_valid = 1;
 	if (parsing->c_encountered == 1)
-		message_map_invalid(parsing, "Error \n La ligne de param CEILING est présente deux fois\n");
+		message_map_invalid(parsing,
+		"Error \n La ligne de param CEILING est présente deux fois\n");
 	parsing->c_encountered = 1;
-	if(!(ceiling = ft_split(parsing->lign, ',')))
+	if (!(ceiling = ft_split(parsing->lign, ',')))
 		return (0);
-	if(!ceiling[0] | !ceiling[1] | !ceiling[2])
-		message_map_invalid(parsing, "Error \n La ligne de param CEILING n'est pas correctement rédigée \n");
-	if(!(rgb = ft_split(ceiling[0], ' ')))
+	if (!ceiling[0] | !ceiling[1] | !ceiling[2])
+		message_map_invalid(parsing,
+		"Error \n La ligne de param CEILING n'est pas correctement rédigée \n");
+	if (!(rgb = ft_split(ceiling[0], ' ')))
 		return (0);
-	if((!rgb[1]) | (rgb[2] != 0))
-		message_map_invalid(parsing, "Error \n La ligne de param CEILING n'est pas correctement rédigée \n");
-	rgb_tab[0] = ft_atoi(rgb[1]);
-	rgb_tab[1] = ft_atoi(ceiling[1]);
-	rgb_tab[2] = ft_atoi(ceiling[2]);
-	if((rgb_tab[0] < 0 | rgb_tab[0] > 255) | (rgb_tab[1] < 0 | rgb_tab[1] > 255) | (rgb_tab[2] < 0 | rgb_tab[2] > 255))
-		message_map_invalid(parsing, "Error \n Le code RGB de param CEILING n'est pas correctement rédigé \n");
-	free_split(rgb);	
+	ft_parse_ceiling_2(parsing, ceiling, rgb);
+	free_split(rgb);
 	free_split(ceiling);
-	return(1);
+	return (1);
 }
-
