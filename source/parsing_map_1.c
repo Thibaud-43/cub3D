85c380c91530a3b9ce8eb8b3d07455e77d26e84b
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-01-08 12:10:20 +0100

    ok

--- source/parsing_map_1.c
+++ source/parsing_map_1.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:22:09 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/08 10:23:10 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/08 11:57:10 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,32 @@ void	ft_check_ch_map(t_parsing *parsing)
 	i = 0;
 	while (parsing->lign[i])
 	{
-		if (parsing->lign[i] == 'N' | parsing->lign[i] == 'S' | parsing->lign[i] == 'E' | parsing->lign[i] == 'W')
+		if (parsing->lign[i] == 'N' | parsing->lign[i] == 'S'
+			| parsing->lign[i] == 'E' | parsing->lign[i] == 'W')
 		{
 			if (parsing->player_found == 1)
-				message_map_invalid(parsing, "Error\n Il y a plusieurs joueurs sur la map \n");
+				message_map_invalid(parsing,
+				"Error\n Il y a plusieurs joueurs sur la map \n");
 			else
 				parsing->player_found = 1;
 		}
-		else if (!(parsing->lign[i] == ' ' | parsing->lign[i] == '0' | parsing->lign[i] == '1' | parsing->lign[i] == '2'))
-			message_map_invalid(parsing, "Error\n La map est invalide\n");
+		else if (!(parsing->lign[i] == ' ' | parsing->lign[i] == '0'
+				| parsing->lign[i] == '1' | parsing->lign[i] == '2'))
+			message_map_invalid(parsing,
+			"Error\n La map est invalide\n");
 		i++;
 	}
 }
 
-int	ft_parse_map(t_parsing *parsing, int k)
+int		ft_parse_map(t_parsing *parsing, int k)
 {
 	parsing->param_line_valid = 1;
-	if(parsing->map_encountered == 0)
+	if (parsing->map_encountered == 0)
 		parsing->line_num_begin_map = k;
-	if(ft_strlen(parsing->lign) > parsing->line_map_len_max)
+	if (ft_strlen(parsing->lign) > parsing->line_map_len_max)
 		parsing->line_map_len_max = ft_strlen(parsing->lign);
 	parsing->nb_lines_map++;
 	ft_check_ch_map(parsing);
 	parsing->map_encountered = 1;
 	return (1);
 }
-
--- source/parsing_map_2.c
+++ source/parsing_map_2.c
@@ -6,22 +6,30 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:22:09 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/08 10:24:27 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/08 12:10:03 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+void	ft_initialize_map_struc(t_p_map *p_map)
+{
+	p_map->k = 1;
+	p_map->z = 1;
+}
 
 int	ft_init_matrice(t_parsing *parsing)
 {
 	int i;
 
 	i = 0;
-	if(!(parsing->matrice = malloc(sizeof(char *) * (parsing->nb_lines_map + 3))))
+	if (!(parsing->matrice = malloc(sizeof(char *)
+		* (parsing->nb_lines_map + 3))))
 		return (0);
-	while(i < parsing->nb_lines_map + 2)
+	while (i < parsing->nb_lines_map + 2)
 	{
-		if(!(parsing->matrice[i] = malloc(sizeof(char *) * (parsing->line_map_len_max + 3))))
+		if (!(parsing->matrice[i] = malloc(sizeof(char *)
+			* (parsing->line_map_len_max + 3))))
 			return (0);
 		i++;
 	}
@@ -42,50 +50,43 @@ int	ft_init_matrice(t_parsing *parsing)
 	return (1);
 }
 
-
 int	ft_parse_map_advanced(t_parsing *parsing, char **argv)
 {
-	int	fd;
-	int	ret;
-	int	k;
-	int	i;
-	int	z;
+	t_p_map	p_map;	
 
-	k = 1;
-	z = 1;
-	fd = open(argv[1], O_RDONLY);
-	if(fd < 0)
+	ft_initialize_map_struc(&p_map);
+	p_map.fd = open(argv[1], O_RDONLY);
+	if (p_map.fd < 0)
 		return (0);
 	if (ft_init_matrice(parsing) == 0)
 		return (0);
-	while((ret = get_next_line(fd, &parsing->lign)))
+	while ((p_map.ret = get_next_line(p_map.fd, &parsing->lign)))
 	{
-		if (k >= parsing->line_num_begin_map)
+		if (p_map.k >= parsing->line_num_begin_map)
 		{
-			i = 0;
-			parsing->matrice[z][i] = ' ';
-			i++;
-			while (parsing->lign[i - 1])
+			p_map.i = 0;
+			parsing->matrice[p_map.z][p_map.i] = ' ';
+			(p_map.i)++;
+			while (parsing->lign[p_map.i - 1])
 			{
-				parsing->matrice[z][i] = parsing->lign[i - 1];
-				i++;
+				parsing->matrice[p_map.z][p_map.i] = parsing->lign[p_map.i - 1];
+				(p_map.i)++;
 			}
-			while ((i) <= (parsing->line_map_len_max + 1))
+			while ((p_map.i) <= (parsing->line_map_len_max + 1))
 			{
-				parsing->matrice[z][i] = ' ';
-				i++;
+				parsing->matrice[p_map.z][p_map.i] = ' ';
+				(p_map.i)++;
 			}
-			parsing->matrice[z][i] = 0;
-			z++;
+			parsing->matrice[p_map.z][p_map.i] = 0;
+			(p_map.z)++;
 		}
-		free(parsing->lign);		
-		k++;
+		free(parsing->lign);
+		(p_map.k)++;
 	}
-	free(parsing->lign);		
-	parsing->matrice[z + 1] = 0;
+	free(parsing->lign);
+	parsing->matrice[(p_map.z) + 1] = 0;
 	ft_parse_map_advanced_2(parsing);
 	ft_print_matrice(parsing);
-	close(fd);
+	close((p_map.fd));
 	return (1);
 }
-
