663b7e8ffc6a3ea54d715a166acc3e478bee5897
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-01-08 17:54:48 +0100

    ok

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:47:07 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/08 17:53:31 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/08 17:54:46 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,14 @@ int		main(int argc, char **argv)
 	t_map	map;
 	ft_initialize_map(&map);
 	printf("Valeur de retour: %d \n", ft_parser(argc, argv, &map));
-	printf("Valeur de no : %s \n", map->no);
-	printf("Valeur de so : %s \n", map->so);
-	printf("Valeur de ea : %s \n", map->ea);
-	printf("Valeur de we : %s \n", map->we);
-	printf("Valeur de SPRITE : %s \n", map->sprite);
-	printf("Valeur de ceiling : %d / %d / %d \n", map->ceiling[0], map->ceiling[1], map->ceiling[2]);
-	printf("Valeur de floor : %d / %d / %d \n", map->floor[0], map->floor[1], map->floor[2]);
-	printf("Valeur de resolution : %d / %d \n", map->resolution[0], map->resolution[1]);
+	printf("Valeur de no : %s \n", map.no);
+	printf("Valeur de so : %s \n", map.so);
+	printf("Valeur de ea : %s \n", map.ea);
+	printf("Valeur de we : %s \n", map.we);
+	printf("Valeur de SPRITE : %s \n", map.sprite);
+	printf("Valeur de ceiling : %d / %d / %d \n", map.ceiling[0], map.ceiling[1], map.ceiling[2]);
+	printf("Valeur de floor : %d / %d / %d \n", map.floor[0], map.floor[1], map.floor[2]);
+	printf("Valeur de resolution : %d / %d \n", map.resolution[0], map.resolution[1]);
 	ft_free_map(&map);
 	return (1);
 }
