80fb4943cf8541335304ae65cdfd2e68ed6ef3a0
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-01-09 13:26:15 +0100

    ok

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: trouchon <trouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:47:07 by trouchon          #+#    #+#             */
-/*   Updated: 2021/01/09 12:25:32 by trouchon         ###   ########.fr       */
+/*   Updated: 2021/01/09 13:25:13 by trouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,37 @@ void	ft_free_map(t_map *map)
 	ft_print_matrice(map);
 }
 
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
 
-int	main(void)
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int             main(void)
 {
     void    *mlx;
     void    *mlx_win;
+    t_data  img;
 
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }
-
 /*int		main(int argc, char **argv)
 {
 	t_map	map;
